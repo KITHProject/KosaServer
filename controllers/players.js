69bdfadc7d216386fbb7065c881b7a983691d9dd
kicky1 <krzysiekwicki1@gmail.com>
2021-05-03 23:11:29 +0200

    Zmiana wysyania danych z posts na graczy

---
+++ controllers/players.js
@@ -0,0 +1,25 @@
+import PlayerMessage from '../models/playerMessage.js'
+
+export const getPlayers = async (req, res) => {
+    try {
+        const playerMessages = await PlayerMessage.find();
+
+        res.status(200).json(playerMessages);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPlayer = async (req, res) => {
+    const player = req.body;
+
+    const newPlayer = new PlayerMessage(player);
+
+    try {
+        await newPlayer.save();
+
+        res.status(201).json(newPlayer);
+    } catch (error) {
+        res.status(409).json({ message: error.message});
+    }
+}
